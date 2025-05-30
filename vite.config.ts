143197e8789cde3e149e95e0db671e6c1b0d0f5a
sandippaul6644 <sandippaul7318@gmail.com>
2025-05-31 01:51:46 +0530

    hgf

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/looplabs-technology/', 
+  base: '/Looplaabs-main/', // 👈 THIS MUST MATCH your GitHub repo name exactly (case-sensitive)
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
